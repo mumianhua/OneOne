0d4f79e5ab2d20b12050d926bc9a8c3a0abeb840
sophiawang <1975054324@qq.com>
2023-11-01 11:20:42 +0800

    add yml file 2

--- first/src/Test1.java
+++ first/src/Test1.java
@@ -2,6 +2,7 @@ public class Test1 {
   public static void main(String[] args) {
     int i = 1;
     int j = 2;
-    System.out.println(i * j);
+    int k = 3;
+    System.out.println(i * j * k);
   }
 }
